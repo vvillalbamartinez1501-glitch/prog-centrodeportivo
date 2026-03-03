168df1080f6a6da398d894238d0990b7668b94ca
V Villalba Martínez <vvillalbamartinez1501@gmail.com>
2026-03-04 00:23:48 +0100

    test(CentroDeportivo, Main): implementacion de un metodo que ordene el array de entrenadores en función de su id de mayor  a menor

--- src/CentroDeportivo.java
+++ src/CentroDeportivo.java
@@ -131,9 +131,186 @@ public class CentroDeportivo {
         if (posicionEntrenador != -1){
             entrenadorEliminado = entrenadores[posicionEntrenador];
             entrenadores[posicionEntrenador] = null;
+            numeroEntrenadores--;
         }
         return entrenadorEliminado;
     }
+
+    /**
+     * metodo que elimina a un entrenador del array de entrenadores
+     * @param entrenador entrenador a eliminar
+     * @return el entrenador eliminado o false si no lo eliminó
+     */
+    public Entrenador deleteEntrenador(Entrenador entrenador){
+        Entrenador entrenadorEliminado;
+        int posicionEntrenador;
+
+        entrenadorEliminado = buscarEntrenador(entrenador.getId());
+        if (entrenadorEliminado != null){
+            posicionEntrenador = buscarIndiceEntrenador(entrenador.getId());
+            entrenadorEliminado = entrenador;
+            entrenadores[posicionEntrenador] = null;
+            numeroEntrenadores--;
+        }
+        return entrenadorEliminado;
+    }
+
+    /**
+     * este metodo hace que en el array de entrenadores, no haya huecos vacios entre los entrenadores
+     */
+
+    public void juntarEntrenadores(){
+        for (int i = 0 ; i < MAX_ENTRENADORES ; i++){
+            if (entrenadores[i] == null){
+                for (int p = i ; p < MAX_ENTRENADORES ; p++){
+                    if (entrenadores[p] != null){
+                        entrenadores[i] = entrenadores[p];
+                        entrenadores[p] = null;
+                    }
+                }
+            }
+        }
+    }
+
+    public void ordenarEntrenadores2(){
+        juntarEntrenadores();
+        Entrenador entrenadorTemporal = null;
+        int posicionAComprobar;
+
+        for (int i = 0 ; i  < numeroEntrenadores ; i++){
+            if (entrenadores[i] != null){
+                posicionAComprobar = i;
+                for (int p =  posicionAComprobar; entrenadorTemporal == null && p < numeroEntrenadores ; p++){
+                    if  (entrenadores[p] != null){
+                        entrenadorTemporal = entrenadores[p];
+                        while(entrenadores[i].getId() < entrenadorTemporal.getId()){
+                            entrenadores[i] = entrenadores[i+1];
+                            entrenadores[i+1] = entrenadorTemporal;
+                            entrenadorTemporal = null;
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    public void ordenarEntrenadores3(){
+        juntarEntrenadores();
+        Entrenador entrenadorTemporal;
+        int posicionAComprobar;
+        int posicionComparativa;
+        for (int i = 0 ; i <= numeroEntrenadores ; i++){
+            if (entrenadores[i] != null){
+                posicionAComprobar = i;
+                posicionComparativa = posicionAComprobar+1;
+                if (entrenadores[posicionComparativa] != null){
+                    if (entrenadores[posicionAComprobar].getId() > entrenadores[posicionComparativa].getId()){
+                        entrenadorTemporal = entrenadores[posicionAComprobar];
+                        entrenadores[posicionAComprobar] = entrenadores[posicionComparativa];
+                        entrenadores[posicionComparativa] = entrenadorTemporal;
+                    }
+                }
+            }
+        }
+        if (entrenadores[0].getId() < entrenadores[1].getId()){
+            entrenadorTemporal = entrenadores[0];
+            entrenadores[0] = entrenadores[1];
+            entrenadores[1] = entrenadorTemporal;
+        }
+    }
+
+    public void ordenarEntrenadores4(){
+        juntarEntrenadores();
+        Entrenador entrenadorTemporal;
+        int indiceSucesionCortada;
+        do{
+            indiceSucesionCortada = comprobarSucesionMayorAMenor();
+
+                //for (int i = indiceSucesionCortada; i < numeroEntrenadores; i++){
+                    if (indiceSucesionCortada > 0 && entrenadores[indiceSucesionCortada+=1] != null){
+                        if (entrenadores[indiceSucesionCortada].getId() < entrenadores[indiceSucesionCortada +=1].getId()){
+                            entrenadorTemporal = entrenadores[indiceSucesionCortada];
+                            entrenadores[indiceSucesionCortada] = entrenadores[indiceSucesionCortada +=1];
+                            entrenadores[indiceSucesionCortada + 1] = entrenadorTemporal;
+                        }
+                    }
+
+
+
+            indiceSucesionCortada = comprobarSucesionMayorAMenor();
+        } while (indiceSucesionCortada != -1);
+    }
+
+
+    public void ordenarEntrenadores5(){
+        juntarEntrenadores();
+        Entrenador entrenadorTemporal;
+        int indiceSucesionCortada;
+        int indiceAnterior;
+        do{
+            indiceSucesionCortada = comprobarSucesionMayorAMenor() -1;
+            indiceAnterior = indiceSucesionCortada +1;
+
+            if (indiceSucesionCortada > 0 && this.entrenadores[indiceSucesionCortada+=1] != null){
+                entrenadorTemporal = entrenadores[indiceSucesionCortada];
+                this.entrenadores[indiceSucesionCortada] = this.entrenadores[indiceAnterior];
+                this.entrenadores[indiceAnterior] = entrenadorTemporal;
+            }
+
+        } while (indiceSucesionCortada != -1);
+    }
+
+    public void ordenarEntrenadores(){
+        juntarEntrenadores();
+        int posicionSucesionCortada = 0;
+        while(posicionSucesionCortada != -1){
+            posicionSucesionCortada = comprobarSucesionMayorAMenor();
+            if (posicionSucesionCortada != -1){
+                cambiarEntrenadoresDePosicion(posicionSucesionCortada,posicionSucesionCortada+1);
+
+            }
+        }
+    }
+
+    public int comprobarSucesionMayorAMenor(){
+        int sucesionCortada = -1;
+        int posicionAComprobar;
+        for (int i = 0 ; sucesionCortada == -1 && i <= numeroEntrenadores ; i++){
+            posicionAComprobar = i;
+            if (entrenadores[i].getId() < entrenadores[posicionAComprobar].getId()){
+                sucesionCortada = i;
+            }
+        }
+        return sucesionCortada;
+    }
+
+    public void cambiarEntrenadoresDePosicion(int posicion1, int posicion2){
+        Entrenador entrenadorTemporal;
+        entrenadorTemporal = entrenadores[posicion1];
+        entrenadores[posicion1] = entrenadores[posicion2];
+        entrenadores[posicion2] = entrenadorTemporal;
+    }
+
+
+
+    private Entrenador buscarEntrenadorDeMayorId(Entrenador[] entrenadoresTemporal){
+        Entrenador entrenadorDeMayorId = null;
+        for (int i = 0; entrenadorDeMayorId == null && i < entrenadoresTemporal.length ; i++){
+            if (entrenadoresTemporal[i] != null){
+                entrenadorDeMayorId =  entrenadoresTemporal[i];
+            }
+        }
+
+        for (int i = 0; i < entrenadoresTemporal.length ; i++){
+            if (entrenadoresTemporal[i] != null && entrenadoresTemporal[i].getId() > entrenadorDeMayorId.getId()){
+                entrenadorDeMayorId = entrenadoresTemporal[i];
+            }
+        }
+        return entrenadorDeMayorId;
+    }
+
+
+
 
     /**
      * recorre el array de entrenadorees en busca del primer indice cuyo entrenador tenga el id dado como parámetro
--- src/Main.java
+++ src/Main.java
@@ -6,14 +6,21 @@ public class Main {
 
         //    Registrar al menos 3 entrenadores.
         Entrenador pingu = new Entrenador(1,"Abel, Martínez Villalba","Máquinas y cardio",5);
-        Entrenador sthefy = new Entrenador(2,"Sthefany, Villafranca Del Valle Seijas","Cardio",26);
+        Entrenador sthefy = new Entrenador(26,"Sthefany, Villafranca Del Valle Seijas","Cardio",26);
         Entrenador venus = new Entrenador(3,"Venus Emilio","Yoga y pilates",10);
-        Entrenador lamine = new Entrenador(4,"Lamine  Yamal","Fútbol",18);
+        Entrenador lamine = new Entrenador(10,"Lamine Yamal","Fútbol",18);
+        Entrenador pedri = new Entrenador(8,"Pedri, González Lopez","Magia",22);
+        Entrenador fermin = new Entrenador(16,"Fermín López","Cañonazos",20);
+        Entrenador raphinhia = new Entrenador(11,"Don Raphael","Jogo Bonito",50);
+        Entrenador cubarsi = new Entrenador(5,"Pau Cubarsí","Fuera de juego",3);
 
-
-        uaxFC.registrarEntrenador(pingu);
         uaxFC.registrarEntrenador(sthefy);
         uaxFC.registrarEntrenador(venus);
+        uaxFC.registrarEntrenador(pingu);
+        uaxFC.registrarEntrenador(pedri);
+        uaxFC.registrarEntrenador(fermin);
+        uaxFC.registrarEntrenador(raphinhia);
+        uaxFC.registrarEntrenador(cubarsi);
 
         //    Mostrar entrenadores.
         System.out.println(uaxFC.mostrarEntrenadores());
@@ -29,6 +36,19 @@ public class Main {
         //    Probar una búsqueda existente y otra inexistente.
         System.out.println(uaxFC.buscarEntrenador(pingu.getId()));
         System.out.println(uaxFC.buscarEntrenador(lamine.getId()));
+
+        // ordenar por id de mayor a menor
+        System.out.println(uaxFC.mostrarEntrenadores());
+
+
+
+
+
+        uaxFC.ordenarEntrenadores();
+        uaxFC.eliminarEntrenador(venus.getId());
+
+
+        System.out.println(uaxFC.mostrarEntrenadores());
 
     }
 
